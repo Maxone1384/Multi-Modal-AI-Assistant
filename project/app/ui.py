95578e4993c9a5d22e031efa698a28e8bebe5626
Maxone1384 <mchenideh@gmail.com>
2025-11-12 10:29:02 -0800

    --save#22

--- project/app/ui.py
+++ project/app/ui.py
@@ -1,7 +1,7 @@
 from huggingface_hub import InferenceClient
 import gradio as gr
 
-client = InferenceClient("mistralai/Mistral-7B-Instruct", token="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")
+client = InferenceClient("mistralai/Mistral-7B-Instruct", token="")
 
 def chat_response(message, history):
     history = history or []
